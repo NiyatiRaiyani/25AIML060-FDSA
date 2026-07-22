3a5b8ef65340b0aeac7729c30f32598cd6508bdc
NiyatiRaiyani <niyatiraiyani42@gmail.com>
2026-07-22 13:49:54 +0530

    modified:   binaryrecursive.cpp

--- binaryrecursive.cpp
+++ binaryrecursive.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
 
+int binarySearchRecursive(int arr[], int low, int high, int target)
+{
+        if (low > high)
+            return -1;
+        int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            else if (arr[mid] < target)
+
+                return binarySearchRecursive(arr, mid + 1, high, target);
+
+        else
+
+        return binarySearchRecursive(arr, low, mid - 1, target);
+}
+int main()
+{
+    int arr[] = {3, 8, 12, 19, 29, 45, 67, 91};
+    int n = 8;
+    int target = 45;
+
+    int result = binarySearchRecursive(arr, 0, n - 1, target);
+
+    if (result != -1)
+        cout << "Found at index " << result;
+
+    else
+        cout << "Not Found";
+
+    return 0;
+}

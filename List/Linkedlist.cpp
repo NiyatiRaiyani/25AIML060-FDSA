280ec486adac1b7b81cca77ef1136097936163d2
NiyatiRaiyani <niyatiraiyani42@gmail.com>
2026-08-03 20:41:07 +0530

    Add compiled object file for LinkedList implementation
    - Introduced Linkedlist.o, the compiled object file for the LinkedList class.
    - This file contains the binary representation of the LinkedList implementation, including methods for adding and printing nodes.

--- List/Linkedlist.cpp
+++ List/Linkedlist.cpp
@@ -1,24 +1,75 @@
 #include<iostream>
 using namespace std;
 
-class Node {
+class Node 
+{
 public:
     int data;
     Node* next;
 
-    Node(int val) {
+    Node(int val) 
+    {
         data = val;
         next = NULL;
     }
 };
+
+class List 
+{
+    Node* head;
+    Node* tail;
+public:
+    List() 
+    {
+        head = NULL;
+        tail = NULL;
+
+    }
+    void push_front(int val) 
+    {
+        Node* newNode = new Node(val);
+        newNode->next = head;
+        head = newNode;
+        if (tail == NULL) 
+        {
+            tail = newNode;
+        }
+    }
+
+    void push_back(int val) 
+    {
+        Node* newNode = new Node(val);
+        if (tail != NULL) 
+        {
+            tail->next = newNode;
+        }
+        tail = newNode;
+        if (head == NULL) 
+        {
+            head = newNode;
+        }
+    }
+
+    void printList() 
+    {
+        Node* temp = head;
+        while (temp != NULL) 
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
 int main() 
 {
-    Node* head = new Node(10);
-    Node* second = new Node(20);
-    Node* third = new Node(30);
-
-    head->next = second;
-    second->next = third;
-
+    List lst;
+    lst.push_front(10);
+    lst.push_front(20);
+    lst.push_front(30);
+    lst.push_back(40);
+    lst.push_back(50);
+    lst.printList();
     return 0;
 }

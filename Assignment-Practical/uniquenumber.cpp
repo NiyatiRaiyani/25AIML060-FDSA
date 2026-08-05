ebd380f5a18400d04aaae17ce7d716c1568ddcfc
NiyatiRaiyani <niyatiraiyani42@gmail.com>
2026-08-05 13:16:19 +0530

    Add detailed comments and input/output format for unique number detection program

--- Assignment-Practical/uniquenumber.cpp
+++ Assignment-Practical/uniquenumber.cpp
@@ -1,3 +1,36 @@
+/* Write a program to find non repeating element in an array (Unique element)
+
+Input : The number of elements, and an array.
+
+Output: Return non repeating number
+
+Input Format
+
+-- first line contains N no. of array elements --Second line contains N elements separated by space
+
+Constraints
+
+-
+
+Output Format
+
+-- print unique number
+
+Sample Input 0
+
+5
+1 2 3 2 1
+Sample Output 0
+
+3
+Sample Input 1
+
+6
+1 2 3 4 1 5
+Sample Output 1
+
+2 3 4 5*/
+
 #include <iostream>
 using namespace std;
 

fcccc56b32157d6cf3556bf36864a394a145bd92
NiyatiRaiyani <niyatiraiyani42@gmail.com>
2026-08-05 12:37:41 +0530

    Add detailed comments and input/output format to duplicate number detection program

--- Assignment-Practical/duplicatenumber.cpp
+++ Assignment-Practical/duplicatenumber.cpp
@@ -1,3 +1,39 @@
+/* Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears at most twice, return an array of all the integers that appears twice otherwise print -1 for no duplicates.
+
+Input Format
+
+first line contains n
+second line contains space separated n number of array elements
+Constraints
+
+1 <= n <= 10^5
+1 <= a[i] <= n
+Each element in nums appears once or twice.
+Output Format
+
+prints dupicates numbers
+Sample Input 0
+
+3
+1 1 2
+Sample Output 0
+
+1
+Sample Input 1
+
+8
+4 2 3 7 8 3 2 1
+Sample Output 1
+
+2 3
+Sample Input 2
+
+2
+1 2
+Sample Output 2
+
+-1 */
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
